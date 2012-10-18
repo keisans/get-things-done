e7b90cc7d9d7e83d60b9c5b5d83d4b46dac759a0
Ryan Struhl <rwstruhl@gmail.com>
2012-10-18 10:51:53 -0400

    initial commit --fresh rails with rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gtddd::Application.config.secret_token = '06f2bd9671322038774811a42b2fa7303f990108bb6f504e7aebb4ac95672302c72705d5a1e3bc92d7e0b3984befc25d752d354df071d2c447be9dd223968801'
